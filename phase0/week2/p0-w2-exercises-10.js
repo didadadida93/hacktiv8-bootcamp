96cccdde763f59f7e8ba871f702892ea312713e2
didadadida93 <didadadida93@gmail.com>
2019-09-30 13:44:54 +0700

    update week 2 exercises 10

--- phase0/week2/p0-w2-exercises-10.js
+++ phase0/week2/p0-w2-exercises-10.js
@@ -1,6 +1,14 @@
 function bandingkanAngka(angka1, angka2) {
   // you can only write your code here!
-  return angka1 > angka2 ? false : angka1 < angka2 ? true : angka1 === angka2 ? -1 : `something wrong happen when angka1 = ${angka1} & angka2 = ${angka2}`
+  if (angka1 > angka2) {
+    return false
+  } else if (angka1 < angka2) {
+    return true
+  } else if (angka1 === angka2) {
+    return -1
+  } else {
+    `something wrong happen when angka1 = ${angka1} & angka2 = ${angka2}`
+  }
 }
 
 // TEST CASES
